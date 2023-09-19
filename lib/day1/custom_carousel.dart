a1786a438857e3d0cca4700cd8d6199fb012548c
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-19 15:52:32 +0100

    first commit

---
+++ lib/day1/custom_carousel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Custom_Carousel extends StatefulWidget {
+  const Custom_Carousel({super.key});
+
+  @override
+  State<Custom_Carousel> createState() => _Custom_CarouselState();
+}
+
+class _Custom_CarouselState extends State<Custom_Carousel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
