9956296e3d7e4befecfc0ca6a7ce5c8963fbf2a0
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-22 17:53:55 +0100

    Update body.dart

--- lib/day3/body.dart
+++ lib/day3/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class Liquid extends StatefulWidget {
   const Liquid({super.key});
@@ -10,6 +11,12 @@ class Liquid extends StatefulWidget {
 class _LiquidState extends State<Liquid> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Builder(
+        builder: (context){
+          return LiquidSwipe(
+            pages: pages, 
+            fullTransitionValue: 800, waveType: WaveType.circularReveal),
+        }));
   }
 }
