6d8c372ba2bbbd06f1d7479e7e953fc1f38c3552
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-22 17:42:15 +0100

    Updating Day3

---
+++ lib/day3/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Liquid extends StatefulWidget {
+  const Liquid({super.key});
+
+  @override
+  State<Liquid> createState() => _LiquidState();
+}
+
+class _LiquidState extends State<Liquid> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
